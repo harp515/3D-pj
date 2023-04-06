7f5991867074b2d5f991d3bfa509290891a8a7fb
harp515 <128121328+harp515@users.noreply.github.com>
2023-04-06 20:50:44 +0900

    :object:
    move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+
     float hAxis;
     float vAxis;
+    float wDown;
+
 
     Vector3 moveVec;
 
-    void Start()
+    Animator anim;
+
+
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -19,5 +26,13 @@ public class Player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walf");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+        anim.SetBool("IsRun", moveVec != Vector3.zero);
+        anim.SetBool("IsRun", wDown);
     }
 }
