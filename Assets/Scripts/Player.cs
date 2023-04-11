1f15719a1a98feffd9fd4ea95914f08903f16d13
harp515 <128121328+harp515@users.noreply.github.com>
2023-04-11 22:01:02 +0900

    :Object:
    Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,30 +9,62 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
-
+    bool jDown;
+    bool isJump;
 
     Vector3 moveVec;
 
     Animator anim;
 
+    Rigidbody rigid;
+
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
-
+        jDown = Input.GetButtonDown("Jump");
+    }
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("IsRun", moveVec != Vector3.zero);
         anim.SetBool("IsWalk", wDown);
-
+    }
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+    void Jump()
+    {
+        if (jDown && !isJump) {
+            rigid.AddForce(Vector3.up * 15,ForceMode.Impulse);   //AddForce : 물리적인 힘을 가한다
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor") {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
