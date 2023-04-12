84370eccba08d017fd7ff756541ae8a3ff68feae
harp515 <128121328+harp515@users.noreply.github.com>
2023-04-12 20:50:25 +0900

    :Object:
    Dodge, Item

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,9 +10,12 @@ public class Player : MonoBehaviour
     float vAxis;
     bool wDown;
     bool jDown;
+
     bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 DodgeVec;
 
     Animator anim;
 
@@ -30,6 +33,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
     }
     void GetInput()
     {
@@ -42,6 +46,10 @@ public class Player : MonoBehaviour
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
+        if(isDodge) {
+            moveVec = DodgeVec;
+        }
+
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("IsRun", moveVec != Vector3.zero);
@@ -53,12 +61,28 @@ public class Player : MonoBehaviour
     }
     void Jump()
     {
-        if (jDown && !isJump) {
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge) {
             rigid.AddForce(Vector3.up * 15,ForceMode.Impulse);   //AddForce : 물리적인 힘을 가한다
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
             isJump = true;
         }
+    }
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge) {
+            DodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+    void DodgeOut()
+    {
+        speed /= 2;
+        isDodge = false;
     }
     void OnCollisionEnter(Collision collision)
     {
